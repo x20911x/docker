2cd3fe453cac342ecc0a4cb51e71410c400fc4bd
Sam <x20911x>
2021-06-24 09:22:45 +0800

    NEW: add redis to count visit times

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import time
+import redis
 from flask import Flask
 
 app = Flask(__name__)
+cache = redis.Redis(host='127.0.0.1', port=16379, password='1234qwer')
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+def get_index():
+    count = get_hit_count()
+    return f'<br><br><br><br><br><br><br><br><br><h1 style="color:#f67;font-size:100px">The {count} time visit!!!!!!!!</h1>'
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8888)
+    app.run(debug=False, host='0.0.0.0', port=8888)
